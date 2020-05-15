ddb9a7a9bac9a9ec3e29f819b66f2b4ce119ccb8
lspacagn <lee@spacagna.co.uk>
2020-05-15 11:36:05 +0100

    Add Babel

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _nodeFetch = require('node-fetch');
+
+var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-
+import _ from 'lodash'
+import fetch from 'node-fetch'
+import fs from 'fs'
+// import analytics from './analytics.js'
